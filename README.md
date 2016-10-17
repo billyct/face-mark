f5eaa9dc29ff0c9f18600fb931495d10ac711030
billyct <billy_allen@126.com>
2016-10-18 01:58:55 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,31 @@
+# image-marker
+
+> mark image part with text
+
+background details relevant to understanding what this module does
+
+## Usage && API
+
+```js
+var ImageMarker = require('image-marker')
+var imageMarker = new ImageMarker('image_url');
+imageMarker.mark({}, function(data) {
+	// do what you like with data
+});
+```
+
+## Install
+
+With [npm](https://npmjs.org/) installed, run
+
+```
+$ npm install image-marker
+```
+
+## Acknowledgments
+
+image-marker was inspired by [HTML5-ASCII-art-converter](http://haruatari.github.io/HTML5-ASCII-art-converter/)
+
+## License
+
+MIT @[billyct](http://billyct.com)
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,14 @@ var path = require('path');
 var webpack = require('webpack');
 
 var libraryName = 'image-marker';
-var outputFile = libraryName + '.js';
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var env = process.env.WEBPACK_ENV;
-var plugins = [];
-
-if (env === 'prod') {
-	plugins.push(new UglifyJsPlugin({minimize: true}));
-	outputFile = libraryName + '.min.js';
-}
 
 var config = {
 	entry: path.resolve(__dirname, 'src/index.js'),
 	devtool: 'source-map',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: outputFile,
+		filename: libraryName + '.js',
 		library: libraryName,
 		libraryTarget: 'umd',
 		umdNamedDefine: true
@@ -33,7 +25,12 @@ var config = {
 		root: path.resolve('./src'),
 		extensions: ['', '.js']
 	},
-	plugins: plugins
+	plugins: []
 };
+
+if (env === 'prod') {
+	config.plugins.push(new webpack.optimize.UglifyJsPlugin({minimize: true}));
+	config.output.filename = libraryName + '.min.js';
+}
 
 module.exports = config;
